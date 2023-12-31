c25cfde4028b9645175fa7590727ce0cdb5e10b6
Danish_Shaikh <89991677+Kaneryaa@users.noreply.github.com>
2023-12-31 14:19:12 +0530

    Update main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,10 +1,25 @@
 provider "aws" {
-  region = "us-east-1"
+    region = "us-east-1"  # Set your desired AWS region
 }
 
-module "ec2_instance" {
-  source = "./modules/ec2_instance"
-  ami_value = "ami-053b0d53c279acc90" # replace this
-  instance_type_value = "t2.micro"
-  subnet_id_value = "subnet-019ea91ed9b5252e7". # replace this
+resource "aws_instance" "example" {
+    ami           = "ami-0c7217cdde317cfec"  # Specify an appropriate AMI ID
+    instance_type = "t2.medium"
+    subnet_id = "subnet-037547f5f0c405570"
+    key_name = "k8s-keyPair"
+
+}
+resource "aws_instance" "node_js" {
+    ami           = "ami-0c7217cdde317cfec"
+    instance_type = "t2.medium"
+    subnet_id     = "subnet-037547f5f0c405570"
+    key_name      = "k8s-keyPair"
+    security_groups = ["sg-07a6a419e96131832"]
+
+    ebs_block_device {
+        device_name = "/dev/sdf"
+        volume_size = 20
+        volume_type = "gp3"  # Specify the desired volume type (e.g., gp2, io1, etc.)
+        # Other optional settings can be added here
+    }
 }
